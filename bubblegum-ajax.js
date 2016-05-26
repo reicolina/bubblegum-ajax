25fa70cd62777d6984f04eecaedbd8a8634d6bb5
Rei Colina <reinaldo13@gmail.com>
2016-05-26 06:15:48 -0700

    Create bubblegum-ajax.js

---
+++ bubblegum-ajax.js
@@ -0,0 +1,28 @@
+var bgAjax = function() {
+  var xhr = new XMLHttpRequest();
+  return function(method, url, payload, callback, isCrossDomain ) {
+    if (isCrossDomain && xhr.withCredentials === undefined) {
+      // For cross-origin requests in IE8 and IE9
+      xhr = new XDomainRequest();
+    }
+    xhr.open( method, url );
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            callback(JSON.parse(xhr.responseText));
+        }
+    };
+    if (payload) {
+      xhr.send(JSON.stringify(payload));
+    } else {
+      xhr.send();
+    }
+  };
+}();
+
+/*
+// test a PUT request using cross domain (CORS)
+bgAjax('put', 'http://xxx.com', {name: 'rei', age: 37}, function(data) {
+    console.log(data);
+}, true);
+*/
